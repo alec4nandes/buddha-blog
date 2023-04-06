dc73036ddc4035f37650a11bc35e3fac6d0a8518
Alec <alec4nandes@gmail.com>
2023-04-06 16:52:25 -0700

    add 404.html

--- src/post.js
+++ src/post.js
@@ -17,5 +17,48 @@ export default async function loadPost() {
         suttaSection.innerHTML = highlightAll(sutta);
         postSection.innerHTML = getSinglePostHTML(draft);
         addAnnotationJumpButtons(sutta, true);
+    } else {
+        window.location.href = "/404.html";
+    }
+
+    function getSuttaInfoHTML() {
+        const {
+            sutta_title,
+            section_pali,
+            chapter,
+            chapter_title,
+            chapter_description,
+            sutta_description,
+            section_title,
+            section_description,
+        } = sutta;
+        return `
+            <details>
+                <summary>
+                    ${[...new Set([section_pali, section_title])].join(": ")}
+                </summary>
+                <p>${section_description}</p>
+                ${
+                    chapter_title && chapter_description
+                        ? `
+                            <p><u>${chapter_title.trim()}</u>:</p>
+                            <p>${chapter_description}</p>
+                        `
+                        : ""
+                }
+            </details>
+            <h1>${sutta_title}</h1>
+            <h2>(${section_pali}${chapter ? " " + chapter : ""})</h2>
+            ${
+                sutta_description
+                    ? `
+                        <details>
+                            <summary>Summary</summary>
+                            <p>${sutta_description}</p>
+                        </details>
+                    `
+                    : ""
+            }
+        `;
     }
 }
