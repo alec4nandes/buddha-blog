aba47662aa7be34d4657cc23f9dcd10588d7dd7a
Alec <alec4nandes@gmail.com>
2023-04-25 12:55:37 -0700

    add about page

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
-const functions = require("firebase-functions"),
-    exec = require("child_process").exec;
+// const functions = require("firebase-functions"),
+//     exec = require("child_process").exec;
 
-const onCreateDraft = functions.firestore.document("drafts/{draftId}").onCreate(
-    // print directory to gcloud console.
-    // just snooping gcloud's file structure...
-    exec("ls -a", function (error, stdout, stderr) {
-        console.log("stdout: " + stdout);
-        console.log("stderr: " + stderr);
-        if (error !== null) {
-            console.log("exec error: " + error);
-        }
-    })
-);
+// const onCreateDraft = functions.firestore.document("drafts/{draftId}").onCreate(
+//     // print directory to gcloud console.
+//     // just snooping gcloud's file structure...
+//     exec("ls -a", function (error, stdout, stderr) {
+//         console.log("stdout: " + stdout);
+//         console.log("stderr: " + stderr);
+//         if (error !== null) {
+//             console.log("exec error: " + error);
+//         }
+//     })
+// );
 
-module.exports = {
-    // onCreateDraft,
-};
+// module.exports = {
+//     onCreateDraft,
+// };
